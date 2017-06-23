cb8da9f485d37c0773baf3fd1d3fbf037c0ade45
Alex Sahm <alexander.sahm@bisnode.com>
2017-06-23 10:27:07 +0200

    added rest and config folder

---
+++ src/main/java/com/bisnode/bhc/configuration/Config.java
@@ -0,0 +1,13 @@
+package com.bisnode.bhc.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by sahm on 21.06.17.
+ */
+
+@Configuration
+public class Config {
+
+}
---
+++ src/main/java/com/bisnode/bhc/rest/GetPortfolioController.java
@@ -0,0 +1,35 @@
+package com.bisnode.bhc.rest;
+
+/**
+ * Created by sahm on 21.06.17.
+ */
+
+import com.bisnode.bhc.configuration.Config;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api
+@RequestMapping("/file")
+public class GetPortfolioController {
+
+    @Autowired
+    public GetPortfolioController(Config config){
+    }
+
+    @ApiOperation(value = "Return portfolio")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return portfolio", response = String.class),
+            @ApiResponse(code = 404, message = "When no portfolio found", response = String.class)})
+
+
+    @RequestMapping(path = "/portfolio/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity getPortfolio(
+            @ApiParam(value = "pdf file to be returned") final @PathVariable("id") String id) {
+        return null;
+    }
+}
+
