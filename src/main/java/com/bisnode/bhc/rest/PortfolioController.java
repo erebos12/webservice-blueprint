b38e9840b0593d91bf89fdab9d390c12f28dadbd
Alex Sahm <alexander.sahm@bisnode.com>
2017-08-21 09:06:59 +0200

    added logger in controller

--- src/main/java/com/bisnode/bhc/rest/PortfolioController.java
+++ src/main/java/com/bisnode/bhc/rest/PortfolioController.java
@@ -4,8 +4,11 @@ package com.bisnode.bhc.rest;
  * Created by sahm on 21.06.17.
  */
 
+import com.bisnode.bhc.BhcWsMain;
 import com.bisnode.bhc.configuration.Config;
 import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @Api
 @RequestMapping("/portfolios")
 public class PortfolioController implements PortfolioApi {
+
+    private static final Logger logger = LoggerFactory.getLogger(PortfolioController.class);
 
     @Autowired
     public PortfolioController(Config config) {
@@ -26,6 +31,7 @@ public class PortfolioController implements PortfolioApi {
     @Override
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity postPortfolio() {
+        logger.info("Receiving POST request...");
         return ResponseEntity.ok(new String("Hello from Portfolio Webservice from Bisnode!"));
     }
 }
