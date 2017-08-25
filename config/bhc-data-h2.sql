dc32608608e14f2b73e899151db95e9c9d87e61a
Alex Sahm <alexander.sahm@bisnode.com>
2017-08-25 09:12:47 +0200

    description for Portfoliio table

--- config/bhc-data-h2.sql
+++ config/bhc-data-h2.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `Portfolio` (	
-	pfl_id int(20) NOT NULL AUTO_INCREMENT,
-	pfl_csg_id int(11) DEFAULT NULL,
-	pfl_wrk_id int(20) DEFAULT NULL,
-	pfl_cust_identifier varchar(20) NOT NULL,
-	pfl_ext_identifier varchar(20) NOT NULL,
-	pfl_dtt_id int(11) NOT NULL,
+	pfl_id int(20) NOT NULL AUTO_INCREMENT, /* internal identifier for a portfolio */
+	pfl_csg_id int(11) DEFAULT NULL, /* = Portfolio-Owner-ID e.g. PBC, P2R */
+	pfl_wrk_id int(20) DEFAULT NULL, /* constant value for backend-processes */
+	pfl_cust_identifier varchar(20) NOT NULL, /* = DUNS-Number */
+	pfl_ext_identifier varchar(20) NOT NULL, /* = Business-Partner-ID (BP_ID) */
+	pfl_dtt_id int(11) NOT NULL, /* = Profile-ID e.g. S,M,L */
 	pfl_country_iso2 char(2) NOT NULL,
-	pfl_strt_dt datetime NOT NULL,
-	pfl_end_dt datetime DEFAULT NULL,
+	pfl_strt_dt datetime NOT NULL, /* beginning of activation */
+	pfl_end_dt datetime DEFAULT NULL, /* beginning of activation. in case its not null, it disables a portfolio */
 	PRIMARY KEY (pfl_id),
 	UNIQUE KEY portfolio_pk (pfl_id)
 );
