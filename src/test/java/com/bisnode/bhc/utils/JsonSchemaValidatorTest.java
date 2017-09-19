134047f7b08c02327801408f556c4a8cc02ae747
Alex Sahm <alexander.sahm@bisnode.com>
2017-09-19 14:39:02 +0200

    cleanup test

--- src/test/java/com/bisnode/bhc/utils/JsonSchemaValidatorTest.java
+++ src/test/java/com/bisnode/bhc/utils/JsonSchemaValidatorTest.java
@@ -2,7 +2,6 @@ package com.bisnode.bhc.utils;
 
 import com.bisnode.bhc.domain.exception.InvalidPortfolioMessageException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.core.util.AsJson;
@@ -15,7 +14,6 @@ import java.io.IOException;
 public class JsonSchemaValidatorTest {
 
     JsonSchemaValidator jsonSchemaValidator = new JsonSchemaValidator();
-    ObjectMapper mapper = new ObjectMapper();
 
     @Before
     public void setup() throws IOException {
