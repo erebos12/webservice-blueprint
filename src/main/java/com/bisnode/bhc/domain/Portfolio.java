ba3582cb21062f028aedd6315b60abed43fd6414
Alex Sahm <alexander.sahm@bisnode.com>
2017-09-22 08:32:51 +0200

    don't use primitive types and add nullable

--- src/main/java/com/bisnode/bhc/domain/Portfolio.java
+++ src/main/java/com/bisnode/bhc/domain/Portfolio.java
@@ -13,31 +13,31 @@ public class Portfolio {
 
     @Id
     @GeneratedValue(strategy=IDENTITY)
-    @Column(name = "pfl_id")
-    public int pfl_id;
+    @Column(name = "pfl_id", nullable = false)
+    public Integer pfl_id;
 
-    @Column(name = "pfl_csg_id")
-    public int pfl_csg_id;
+    @Column(name = "pfl_csg_id", nullable = true)
+    public Integer pfl_csg_id;
 
-    @Column(name = "pfl_wrk_id")
-    public int pfl_wrk_id;
+    @Column(name = "pfl_wrk_id", nullable = true)
+    public Integer pfl_wrk_id;
 
-    @Column(name = "pfl_cust_identifier")
+    @Column(name = "pfl_cust_identifier", nullable = false)
     public String pfl_cust_identifier;
 
-    @Column(name = "pfl_ext_identifier")
-    public int pfl_ext_identifier;
+    @Column(name = "pfl_ext_identifier", nullable = false)
+    public Integer pfl_ext_identifier;
 
-    @Column(name = "pfl_dtt_id")
-    public int pfl_dtt_id;
+    @Column(name = "pfl_dtt_id", nullable = false)
+    public Integer pfl_dtt_id;
 
-    @Column(name = "pfl_country_iso2")
+    @Column(name = "pfl_country_iso2", nullable = false)
     public String pfl_country_iso2;
 
-    @Column(name = "pfl_strt_dt")
+    @Column(name = "pfl_strt_dt", nullable = false)
     public Date pfl_strt_dt;
 
-    @Column(name = "pfl_end_dt")
+    @Column(name = "pfl_end_dt", nullable = true)
     public Date pfl_end_dt;
 
     public String toString() {
