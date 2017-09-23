e1c7913e1402a7279c2d632a4bcc970ac0f7b4fb
Alex Sahm <alexander.sahm@bisnode.com>
2017-09-23 23:04:28 +0200

    added persistence-unit for production

--- src/main/java/com/bisnode/bhc/infrastructure/TableSelector.java
+++ src/main/java/com/bisnode/bhc/infrastructure/TableSelector.java
@@ -1,8 +1,5 @@
 package com.bisnode.bhc.infrastructure;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -13,7 +10,6 @@ import java.util.List;
 
 public class TableSelector {
 
-    private static final Logger logger = LoggerFactory.getLogger(PortfolioDbOperator.class);
     private EntityManagerFactory emf;
     private EntityManager em;
 
