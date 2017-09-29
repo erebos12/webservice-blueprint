ee663b407fc0734df0bb69b3cd10385d477ab81e
Alex Sahm <alexander.sahm@bisnode.com>
2017-09-29 08:49:46 +0200

    now 500 is sent instead of 400

--- src/main/java/com/bisnode/bhc/rest/PostPortfolioApi.java
+++ src/main/java/com/bisnode/bhc/rest/PostPortfolioApi.java
@@ -19,7 +19,7 @@ public interface PostPortfolioApi {
     @ApiOperation(value = "Post a portfolio")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Portfolio posted successfully", response = String.class),
-            @ApiResponse(code = 400, message = "Portfolio message content is invalid", response = String.class)})
+            @ApiResponse(code = 500, message = "Portfolio message content is invalid", response = String.class)})
 
     public ResponseEntity<?> postPortfolio(final @RequestBody String body,
                                            final @RequestParam(value = "active", required = false) String active) throws IOException;
